a0214cd75706c39e14f0f0d03bed472337870114
Joey Palchak <joey.palchak@gmail.com>
2023-09-28 11:50:54 -0700

    create Game class

---
+++ WordPuzzle/Models/Game.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System;
+
+namespace WordPuzzle.Models
+{
+  public class Game
+  {
+    private static List<Game> _instances = new List<Game>{};
+    public Puzzle Puzzle { get; set; }
+    // public List<Puzzle> Puzzles { get; set; }
+    public int Id { get; set; }
+    public string Type { get; set; }
+
+    public Game()
+    {
+      _instances.Add(this);
+      Id = _instances.Count;
+
+      Puzzle = new Puzzle();
+      
+      Type = "Random";
+    }
+
+    public Game(string chosenWord)
+    {
+      _instances.Add(this);
+      Id = _instances.Count;
+
+      Puzzle = new Puzzle(chosenWord);
+
+      Type = "Custom";
+    }
+
+    public static Game Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
